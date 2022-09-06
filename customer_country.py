d31535fba1e43a79d8022614ff1ac7bd862cd780
owendecicco1 <owen_decicco1@baylor.edu>
2022-09-06 12:58:03 -0500

    Customer country + csv

--- customer_country.py
+++ customer_country.py
@@ -2,7 +2,24 @@ def main():
     import csv
 
     infile = open("customers.csv","r")
+    outfile = open("customer_country.csv","w")
 
-    outfile = open("customer_county.csv","w")
+    csvfile = csv.reader(infile,delimiter=",")
+
+    next(csvfile)
+
+    outfile.write("Full Name, Country\n")
+
+    for record in csvfile:
+        full_name = record[1] + " " + record[2]
+        country = record[4]
+
+        outfile.write(full_name + "," + country + "\n")
+
+    outfile.close()
+
+
+main()
+
 
     

0db31122672d5a93885eb48ca2e2861133dcf5f8
owendecicco1 <owen_decicco1@baylor.edu>
2022-09-08 18:38:19 -0500

    Finale

--- avg_steps.py
+++ avg_steps.py
@@ -2,11 +2,36 @@ def main():
     import csv 
 
     infile = open("steps.csv","r")
-    outfile = open("avg_steps","w")
-
+    outfile = open("avg_steps.csv","w")
     csvfile = csv.reader(infile,delimiter=",")
-    infile.readline()
 
-    new_list = []
+    next(csvfile)
+    months = ['Void', 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
     month = 1
-    for record in csv_file:
+    steps = 0
+    days = 0
+    for record in csvfile:
+
+        if record[0] == str(month):
+            steps += int(record[1])
+            days += 1
+
+        else:
+            average_steps = format(steps/days, '.2f')
+            outfile.write(months[int(month)] + ', ' + str(average_steps) + '\n')
+            month = record[0]
+            steps = int(record[1])
+            days = 1
+    
+    average_steps = format(steps/days, '.2f')
+    outfile.write(months[int(month)] + ', ' + str(average_steps) + '\n')
+    month = record[0]
+    steps = int(record[1])
+    days = 1
+
+    
+    outfile.close()
+
+main()
+
+
